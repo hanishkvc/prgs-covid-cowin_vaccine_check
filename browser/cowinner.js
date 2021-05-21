299d2823bf3e28730ec8740f674b65c24ce87342
hanishkvc <hanishkvc@gmail.com>
2021-05-22 04:24:27 +0530

    Initial take at getting vaccine center details wrt districts

--- browser/cowinner.js
+++ browser/cowinner.js
@@ -2,6 +2,31 @@
 const srvr = "https://cdn-api.co-vin.in/api";
 var goStates = null;
 var elMain = document.getElementById("main");
+var gDate = "22-05-2021";
+var gState = "KERALA";
+
+
+function get_vaccenters(districtId, el, date=null) {
+	if (date === null) date = gDate;
+	fetch(`${srvr}/v2/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${date}`)
+		.then(resp => resp.json())
+		.then((oVCs) => {
+			oVCs.sessions.forEach(vc => {
+				let tP = document.createElement("p");
+				let vType = '???';
+				if (vc.vaccine === "COVISHIELD")
+					vType = 'SSS';
+				else if (vc.vaccine === "COVAXIN")
+					vType = 'VVV';
+				tP.textContent = `>>> ${vType} ${vc.available_capacity} [${vc.name}, ${vc.pincode}] ${vc.min_age_limit}`;
+				el.appendChild(tP);
+			});
+		})
+		.catch((error) => {
+			console.error(error)
+			el.innerHTML = "<h2>ERROR:Fetching Vaccine centers</h2>";
+		});
+}
 
 
 function get_districts(stateId, el) {
@@ -10,8 +35,11 @@ function get_districts(stateId, el) {
 		.then((oDists) => {
 			oDists.districts.forEach(dist => {
 				let tP = document.createElement("p");
-				tP.textContent = `+ [${dist.district_id}] ${dist.district_name}`;
+				tP.textContent = `>>> [${dist.district_id}] ${dist.district_name}`;
 				el.appendChild(tP);
+				let tChild = document.createElement("div");
+				el.appendChild(tChild);
+				get_vaccenters(dist.district_id, tChild);
 			});
 		})
 		.catch((error) => {
@@ -30,6 +58,7 @@ function get_states(el) {
 				let tP = document.createElement("p");
 				tP.textContent = `[${state.state_id}] ${state.state_name}`;
 				el.appendChild(tP);
+				if (state.state_name.toUpperCase() !== gState) continue;
 				let tChild = document.createElement("div");
 				el.appendChild(tChild);
 				get_districts(state.state_id, tChild);
