385414d07dbe340669b2e4f0af28695132b2bc23
hanishkvc <hanishkvc@gmail.com>
2021-05-22 03:49:21 +0530

    Try fetch districts beloning to each state
    i.e using the CoWin public api.

--- browser/cowinner.js
+++ browser/cowinner.js
@@ -4,15 +4,35 @@ var goStates = null;
 var elMain = document.getElementById("main");
 
 
+function get_districts(stateId, el) {
+	fetch(`${srvr}/v2/admin/location/districts/${stateId}`)
+		.then(resp => resp.json())
+		.then((oDists) => {
+			oDists.districts.forEach(dist => {
+				let tP = document.createElement("p");
+				tP.textContent = `+ [${dist.district_id}] ${dist.district_name}`;
+				el.appendChild(tP);
+			});
+		})
+		.catch((error) => {
+			console.error(error)
+			el.innerHTML = "<h2>ERROR:Fetching Districts</h2>";
+		});
+}
+
+
 function get_states(el) {
 	fetch(`${srvr}/v2/admin/location/states`)
 		.then(resp => resp.json())
 		.then((data) => {
 			goStates = data;
 			goStates.states.forEach(state => {
-				tP = document.createElement("p");
+				let tP = document.createElement("p");
 				tP.textContent = `[${state.state_id}] ${state.state_name}`;
 				el.appendChild(tP);
+				let tChild = document.createElement("div");
+				el.appendChild(tChild);
+				get_districts(state.state_id, tChild);
 			});
 		})
 		.catch((error) => {
