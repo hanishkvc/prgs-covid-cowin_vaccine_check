dbad924951fd009e5f77195866623ddd47c58df0
hanishkvc <hanishkvc@gmail.com>
2021-05-22 18:48:17 +0530

    NodeJS: commandline args for nodejs based version
    Now state and date can be specified on the commandline.

--- nodejs/index.js
+++ nodejs/index.js
@@ -6,9 +6,32 @@
 
 fetch = require('node-fetch');
 const cw = require('../browser/cowinner');
+var gDate = null;
+var gState = 'Kerala';
 
-var db = { 'date': '22-05-2021' };
-cw.dbget_states(db, [ 'KERALA' ])
 
-console.log(db);
+function handle_args(cmdArgs) {
+	for(i=0; i < cmdArgs.length;) {
+		if (cmdArgs[i] === '--state') {
+			gState = cmdArgs[i+1];
+			i += 1;
+		}
+		if (cmdArgs[i] === '--date') {
+			gDate = cmdArgs[i+1];
+			i += 1;
+		}
+		i += 1;
+	}
+	if (gDate === null) {
+		tDate = new Date();
+		gDate = `${tDate.getDate()}-${tDate.getMonth()+1}-${tDate.getFullYear()}`;
+	}
+	gState = gState.toUpperCase();
+	console.log(`INFO:State=${gState}:Date=${gDate}`);
+}
+
+
+handle_args(process.argv.slice(2));
+var db = { 'date': gDate };
+cw.dbget_states(db, [ gState ])
 
