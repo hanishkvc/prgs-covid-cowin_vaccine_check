c7148a9b6818c3ff1166971ce192889e1e0dc38a
hanishkvc <hanishkvc@gmail.com>
2021-05-25 23:45:09 +0530

    MBrowserVer: Cleaner Done message including a oversight fix

--- browser/main.js
+++ browser/main.js
@@ -105,8 +105,8 @@ function do_search(bNotifyMode) {
 	dbget_states(db)
 		.then(() => {
 			show_vcs(elMainTbl, db);
-			msg = `State: ${db.states} Date: ${db.date} Vac: ${db.vaccine} NumOfVacCenters: ${db.vcCnt}`
-			update_status("Done:"+msg);
+			msg = `State: ${db.s_states}, Date: ${db.date}, Vac: ${db.vaccine}, NumOfVacCenters: ${db.vcCnt}`
+			update_status("Done: "+msg);
 			if (bNotifyMode && db.bNotifyMe) notify_user(msg);
 		});
 }
